b036c094e8ce6f0c5a13358801af4dd6bfa0b795
P.Robin <47240170+yoskama26@users.noreply.github.com>
2023-06-28 16:00:28 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send('slt cv');
-  });
-  const port = 3000; // Port sur lequel le serveur écoutera les requêtes
+console.log(`Test`);
 
-  app.listen(port, () => {
-    console.log(`Le serveur est en écoute sur le port ${port}`);
-  });
   
