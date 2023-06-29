e26471351f94eb98f9dc2546958f7b7f4ae306dd
P.Robin <47240170+yoskama26@users.noreply.github.com>
2023-06-29 14:44:23 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,10 @@
-console.log(`Test`);
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Salut');
+});
+
+app.listen(3000, () => {
+  console.log('Serveur en cours d\'exécution sur le port 3000');
+});
